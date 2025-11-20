08c1ca223b5014e2644758199e321754b2ee9734
S-Ali-Haider <s_ali_haider@outlook.com>
2025-11-20 01:41:59 +0000

    test: add first basic failing test when there are no scanned items (failing). Add minimal code (checkout interface + classes).

---
+++ CheckoutKata.Core/Checkout.cs
@@ -0,0 +1,24 @@
+﻿using Checkout_Kata;
+
+namespace CheckoutKata.Core
+{
+    public class Checkout : ICheckout
+    {
+        private readonly Rules _rules;
+
+        public Checkout(Rules rules)
+        {
+            _rules = rules;
+        }
+
+        public void Scan(string item)
+        {
+
+        }
+
+        public int GetTotalPrice()
+        {
+            return -1;
+        }
+    }
+}
---
+++ CheckoutKata.Core/ICheckout.cs
@@ -0,0 +1,8 @@
+﻿namespace Checkout_Kata
+{
+    public interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+}
